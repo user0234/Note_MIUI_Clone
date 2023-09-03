441a73d0a50ea43cd996f82906ff86271d5f0803
nouser3 <heyiamhere05@gmail.com>
2023-09-03 13:41:26 +0530

    added the code for the description item in the note

--- app/src/main/java/com/hellow/notemiuiclone/utils/TestModels.kt
+++ app/src/main/java/com/hellow/notemiuiclone/utils/TestModels.kt
@@ -9,30 +9,30 @@ import java.time.format.DateTimeFormatter
 
 object TestModels {
 
-    val note1Desc1:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"text one",false,null)
-    val note1Desc2:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"test second",false,null)
-    val note1Desc3:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"text three",false,null)
-
-    val noteDesItemList: List<NoteDescItem> = listOf<NoteDescItem>(note1Desc1,note1Desc2,note1Desc3)
-    val note1:NoteItem = NoteItem("id1","Note Item 1 ","none",noteDesItemList,0,NoteStatus.Alive,
-        LocalDateTime.now().format(
-        DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm:ss a")).toString()
-    )
-
-    val note2Desc1:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"text one",false,null)
-    val note2Desc2:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"test second",false,null)
-    val note2Desc3:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"text three",false,null)
-
-
-    val note2:NoteItem = NoteItem("id2","Note Item 2 ","none",noteDesItemList,0,NoteStatus.Alive,LocalDateTime.now().format(
-        DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm:ss a")).toString())
-
-    val note3Desc1:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"text one",false,null )
-    val note3Desc2:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"test second",false,null   )
-    val note3Desc3:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"text three",false,null )
-
-
-    val note3:NoteItem = NoteItem("id3","Note Item 3 ","none",noteDesItemList,0,NoteStatus.Alive,LocalDateTime.now().format(
-        DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm:ss a")).toString())
+//    val note1Desc1:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"text one",false,null)
+//    val note1Desc2:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"test second",false,null)
+//    val note1Desc3:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"text three",false,null)
+//
+//    val noteDesItemList: List<NoteDescItem> = listOf<NoteDescItem>(note1Desc1,note1Desc2,note1Desc3)
+//    val note1:NoteItem = NoteItem("id1","Note Item 1 ","none",noteDesItemList,0,NoteStatus.Alive,
+//        LocalDateTime.now().format(
+//        DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm:ss a")).toString()
+//    )
+//
+//    val note2Desc1:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"text one",false,null)
+//    val note2Desc2:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"test second",false,null)
+//    val note2Desc3:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"text three",false,null)
+//
+//
+//    val note2:NoteItem = NoteItem("id2","Note Item 2 ","none",noteDesItemList,0,NoteStatus.Alive,LocalDateTime.now().format(
+//        DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm:ss a")).toString())
+//
+//    val note3Desc1:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"text one",false,null )
+//    val note3Desc2:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"test second",false,null   )
+//    val note3Desc3:NoteDescItem = NoteDescItem(NoteDescItemType.Text,"text three",false,null )
+//
+//
+//    val note3:NoteItem = NoteItem("id3","Note Item 3 ","none",noteDesItemList,0,NoteStatus.Alive,LocalDateTime.now().format(
+//        DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm:ss a")).toString())
 
 }
